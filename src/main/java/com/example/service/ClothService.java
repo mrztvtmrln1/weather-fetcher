dc5d416182f5ca023363c61ef3311afcdf77e959
myrzataevtemirlan <myrzataevtemirlan76@gmail.com>
2025-06-27 15:19:31 +0500

    added get endpoint to get list of clothes

--- src/main/java/com/example/service/ClothService.java
+++ src/main/java/com/example/service/ClothService.java
@@ -1,11 +1,16 @@
 package com.example.service;
 
+import com.example.enums.ClothBodyType;
 import com.example.model.Cloth;
 import com.example.model.Weather;
 import com.example.repository.ClothRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +27,17 @@ public class ClothService {
        Weather weather = weatherService.getLastWeather(city)
                .orElseThrow(() -> new RuntimeException("Weather not found"));
        boolean isWearableInWind = weather.getWindSpeed() < 5.0;
-       return clothRepository.findByTempRangeAndWind((int)Math.round(weather.getTemperature()),isWearableInWind);
+       List<Cloth> suitableClothes =  clothRepository.findByTempRangeAndWind((int)Math.round(weather.getTemperature()),isWearableInWind);
+       Map<ClothBodyType,Cloth> map = new HashMap<>();
+
+       List<Cloth> lookForDay = new ArrayList<>();
+
+        for (Cloth suitableClothe : suitableClothes) {
+            if (!map.containsKey(suitableClothe.getBodyType())) {
+                map.put(suitableClothe.getBodyType(), suitableClothe);
+                lookForDay.add(suitableClothe);
+            }
+        }
+       return lookForDay;
     }
 }
