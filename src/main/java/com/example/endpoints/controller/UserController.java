8b3c28bc79e86a5dd908e0cdd9bab72af5dcaefa
Мырзатаев Темирлан <tketkev@gmail.com>
2026-03-05 19:31:28 +0500

    CLOTH-10:working logic of endpoint

--- src/main/java/com/example/endpoints/controller/UserController.java
+++ src/main/java/com/example/endpoints/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.example.endpoints.controller;
 
+import com.example.dto.ChangeStatusRequest;
 import com.example.model.User;
 import com.example.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,5 +15,10 @@ public class UserController {
     @PostMapping
     public User addUser(@RequestBody User user) {
         return userService.save(user);
+    }
+
+    @PostMapping("/change-status")
+    public User changeStatus(@RequestBody ChangeStatusRequest changeStatusRequest) {
+        return userService.changeStatus(changeStatusRequest.userId(), changeStatusRequest.newStatus());
     }
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -13,4 +13,11 @@ public class UserService {
     public User save(User user){
         return userRepository.save(user);
     }
+
+    public User changeStatus(Long id, Boolean status){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        user.setStatus(status);
+        return userRepository.save(user);
+    }
 }
